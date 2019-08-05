1f8c347d73af627b2deebd86c9e30172eb93d5a0
Tomáš Pilnaj <pilnaj.t@gmail.com>
2019-08-05 10:19:51 +0200

    setup gtm

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,8 +3,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import withReduxStore from '../lib/with-redux-store';
 import Layout from '../components/Layout';
+import TagManager from 'react-gtm-module'
+ 
+const tagManagerArgs = {
+    gtmId: 'GTM-M684KB'
+}
 
 class MyApp extends App {
+  componentDidMount () {
+    TagManager.initialize(tagManagerArgs)
+  }
   render() {
     const { Component, pageProps, reduxStore } = this.props;
     return (
